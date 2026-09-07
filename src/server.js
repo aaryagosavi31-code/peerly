2f6b18fc405a98fa77fd0ccebeee27927e04a03c
aaryagosavi31-code <aaryagosavi31@gmail.com>
2026-09-07 18:10:10 +0530

    deployment error solved

--- src/server.js
+++ src/server.js
@@ -47,3 +47,10 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
+}
+
+module.exports = app;
